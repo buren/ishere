82744c6be556eb6aa3d57dd934ed509f898f59dc
Jacob Burenstam Linder <burenstam@gmail.com>
2025-05-22 10:35:38 +0200

    Dead simple test for /docs page

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,8 +3,13 @@ import { describe, it, expect } from 'vitest';
 import { homePageHtml } from '../src/html';
 
 describe('IsHere worker', () => {
-	it('responds with Hello World! (integration style)', async () => {
+	it('responds with home page', async () => {
 		const response = await SELF.fetch('https://example.com');
 		expect(await response.text()).toMatch(homePageHtml());
+	});
+
+	it('responds API docs page', async () => {
+		const response = await SELF.fetch('https://example.com/docs');
+		expect(await response.text()).toMatch('Scalar.createApiReference');
 	});
 });
